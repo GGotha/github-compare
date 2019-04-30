aede3951efe4f7f921adfa7064c5e978cfe29c26
Gustavo <snakygamer@gmail.com>
2019-04-30 15:30:54 -0300

    quase pronto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
+import Main from './pages/Main';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Main />
+  </Fragment>
+);
 
 export default App;
